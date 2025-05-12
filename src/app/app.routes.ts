a077ba81e2478b703feb060a608fe91fa31c5d03
seba <komorowski.seba@gmail.com>
2025-05-12 22:47:20 +0200

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ViewStartComponent } from './Views/view-start/view-start.component';
+import { ViewGameComponent} from './Views/view-game/view-game.component';
+import { ViewEndGameComponent} from './Views/view-end-game/view-end-game.component';
+
+export const routes: Routes = [
+  { path: '', component: ViewStartComponent },
+  { path: 'game', component: ViewGameComponent },
+  { path: 'end', component: ViewEndGameComponent },
+  { path: '**', redirectTo: '' } // fallback do startu
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
