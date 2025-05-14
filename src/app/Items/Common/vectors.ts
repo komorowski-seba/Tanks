fe5ff181d93daeec29d7abfcdab5ef9e4f5d19dd
seba <komorowski.seba@gmail.com>
2025-05-14 22:27:49 +0200

    move with directions

---
+++ src/app/Items/Common/vectors.ts
@@ -0,0 +1,4 @@
+export const RIGHT_VECTOR = { x: 1, y: 0 } as const;
+export const LEFT_VECTOR = { x: -1, y: 0 } as const;
+export const UP_VECTOR = { x: 0, y: -1 } as const;
+export const DOWN_VECTOR = { x: 0, y: 1 } as const;
