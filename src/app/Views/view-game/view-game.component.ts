f792bef91f84b54e1f0f5e893e1fe7aff35c9559
seba <komorowski.seba@gmail.com>
2025-06-18 00:48:12 +0200

    update method

--- src/app/Views/view-game/view-game.component.ts
+++ src/app/Views/view-game/view-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import {Tank} from '../../Items/Tank/tank';
 
@@ -9,10 +9,11 @@ import {Tank} from '../../Items/Tank/tank';
   templateUrl: './view-game.component.html',
   styleUrl: './view-game.component.css'
 })
-export class ViewGameComponent implements OnInit {
+export class ViewGameComponent implements OnInit, OnDestroy {
   @ViewChild('sceneContainer', { static: true }) sceneContainer!: ElementRef<HTMLDivElement>;
 
   private player: Tank = new Tank(10, 10, 20, 40);
+  private intervalId: any;
 
   constructor(private physicService: GameService) {
   }
@@ -20,10 +21,21 @@ export class ViewGameComponent implements OnInit {
   ngOnInit() {
     this.physicService.createScene(this.sceneContainer.nativeElement, 800, 700);
     this.physicService.addGameObject(this.player);
+
+    this.intervalId = setInterval(() => {
+      this.update();
+    }, 100);
   };
 
+  ngOnDestroy() {
+    clearInterval(this.intervalId); // żeby nie było wycieku pamięci
+  }
   @HostListener('window:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     this.player.keyEvent(event.key);
+  }
+
+  update(): void {
+
   }
 }
