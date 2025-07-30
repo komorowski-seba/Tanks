cbe63150fdd23ca1d42d6050d491b4029fd639c7
seba <komorowski.seba@gmail.com>
2025-07-30 23:31:11 +0200

    canvas

--- src/app/services/game-service.ts
+++ src/app/services/game-service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@angular/core';
-import { Tank } from '../Items/Tank/tank';
+import {Injectable, NgIterable} from '@angular/core';
+import {Tank} from '../Items/Tank/tank';
+import {Canvas} from '../Items/Canvas/canvas';
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
-  private player: Tank = new Tank(10, 10);
-  private intervalId: any;
-  private boxes: number[] = Array.from({ length: 40 * 40 });
+  private _player: Tank = new Tank(10, 10);
+  private _canvas: Canvas = new Canvas(80);
+
+  get canvas(): NgIterable<any> | null {
+    return this._canvas.canvas
+  }
 
   public keyEvent(key: string): void {
-    this.player.keyEvent(key);
+    this._player.keyEvent(key);
   }
 }
