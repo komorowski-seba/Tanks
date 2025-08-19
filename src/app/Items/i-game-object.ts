cff00b1c73a51bae02c15692d6dddcf1abada41e
seba <komorowski.seba@gmail.com>
2025-08-19 21:33:00 +0200

    canvas color

--- src/app/Items/i-game-object.ts
+++ src/app/Items/i-game-object.ts
@@ -1,7 +1,10 @@
+import {Canvas} from './Canvas/canvas';
+
 export interface IGameObject {
   update(): void;
   keyEvent(key: string): void;
   getX(): number;
   getY(): number;
+  draw(canvas: Canvas): void;
 }
 
--- src/app/services/game-service.ts
+++ src/app/services/game-service.ts
@@ -1,17 +1,28 @@
-import {Injectable, NgIterable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Tank} from '../Items/Tank/tank';
 import {Canvas} from '../Items/Canvas/canvas';
+import {IGameObject} from '../Items/i-game-object';
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
-  private _player: Tank = new Tank(10, 10);
   private _canvas: Canvas = new Canvas(80);
+  private _gameObjects: IGameObject[] = [
+    new Tank(10, 10),
+  ];
 
   get canvas(): Canvas {
     return this._canvas
   }
 
   public keyEvent(key: string): void {
-    this._player.keyEvent(key);
+    for (const gameObject of this._gameObjects) {
+      gameObject.keyEvent(key);
+    }
+  }
+
+  public update(): void {
+    for (const gameObject of this._gameObjects) {
+      gameObject.update();
+    }
   }
 }
